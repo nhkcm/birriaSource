77ca454f1c8f2c85dce49868d0a89a7462613130
jose luis fragozo avendaño <nhkcm@hotmail.com>
2016-11-29 17:25:10 -0500

    commit

---
+++ BirriaSource/BirriasBasic/libs/routes/login.js
@@ -0,0 +1,13 @@
+﻿
+exports.hello = function (req, res) {
+    res.send("hello world!");
+};
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.send("hola mundo desde routes");
+});
+
+module.exports = router;
--- BirriaSource/BirriasBasic/server.js
+++ BirriaSource/BirriasBasic/server.js
@@ -1,5 +1,6 @@
 ﻿
 var express = require('express');
+var login = require('./libs/routes/login');
 var http = require('http');
 var port = 1337;
 
@@ -9,6 +10,16 @@ app.listen(port, function (err) {
     console.log("escuchando desde el puerto");
 });
 
-app.get('/', function (q, s) {
-    s.send("hello world!");
+app.get('/', function (r,s) {
+    s.sendFile(__dirname + "/public/index.html");
 });
+
+app.use('/login', login);
+//app.get('/', function (q, s) {
+//    s.sendFile(__dirname + "/public/index.html");
+//});
+
+//app.get('/', login.hello);
+//app.get('/download', function (req, res) {
+//    res.download(__dirname + "/public/hola_mundo_file.txt");
+//});
